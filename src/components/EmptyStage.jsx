63f0f6fcc467d5284ccda3c4a61941badb35e8c5
Phyo Thura <155300367+K2P4@users.noreply.github.com>
2024-02-01 22:09:27 +0630

    Update EmptyStage.jsx

--- src/components/EmptyStage.jsx
+++ src/components/EmptyStage.jsx
@@ -4,7 +4,12 @@ const EmptyStage = () => {
   
   return (
     <div className='mt-16 mx-auto  mb-2 sm:mb-9 flex flex-col align-middle items-center'>
-        <img src="../../public/product.png" className='' alt="" />
+        <lord-icon
+    src="https://cdn.lordicon.com/eiekfffz.json"
+    trigger="hover"
+    stroke="bold"
+    style="width:250px;height:250px">
+</lord-icon>
         <p className=' text-sm  select-none sm:text-md  mt-8 underline text-gray-700  '>There is no item in cart </p>
 
     </div>
