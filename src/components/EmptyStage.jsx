2f85c3d46fde26d6b1d81686715e702124811c21
Phyo Thura <155300367+K2P4@users.noreply.github.com>
2024-02-01 22:37:32 +0630

    Update EmptyStage.jsx

--- src/components/EmptyStage.jsx
+++ src/components/EmptyStage.jsx
@@ -5,7 +5,12 @@ const EmptyStage = () => {
   return (
     <div className='mt-16 mx-auto  mb-2 sm:mb-9 flex flex-col align-middle items-center'>
 
-      			<img src="../../public/wired-lineal-146-trolley.gif" className="" alt="" />
+      						<img
+				src="https://wireinnovation.com/wp-content/uploads/elementor/thumbs/ecommerce-recolor-p2kdjnj17duzzn4vhi2xk832qijq2ytpumpnu1jfdk.png"
+				className=""
+				alt=""
+			/>
+
 
         <p className=' text-sm  select-none sm:text-md  mt-8 underline text-gray-700  '>There is no item in cart </p>
 
