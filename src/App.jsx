02b14c2b0bed00c3f428b186d6d4ce0980f80869
Phyo Thura <155300367+K2P4@users.noreply.github.com>
2024-02-01 22:41:21 +0630

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import LayoutScreen from "./components/LayoutScreen";
 const App = () => {
 	const { disable } = useContext(ShoppingContext);
 	return (
-		<div className=" px-2 sm:px-0 font-san">
+		<div className=" px-3 sm:px-0 font-san">
 			<LayoutScreen>
 				<Header />
 
