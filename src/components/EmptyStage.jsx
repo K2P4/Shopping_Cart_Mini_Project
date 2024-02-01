4b5546673667db62024775940807882cfb53cf15
Phyo Thura <155300367+K2P4@users.noreply.github.com>
2024-02-01 22:30:03 +0630

    Update EmptyStage.jsx

--- src/components/EmptyStage.jsx
+++ src/components/EmptyStage.jsx
@@ -4,12 +4,9 @@ const EmptyStage = () => {
   
   return (
     <div className='mt-16 mx-auto  mb-2 sm:mb-9 flex flex-col align-middle items-center'>
-        <lord-icon
-    src="https://cdn.lordicon.com/eiekfffz.json"
-    trigger="hover"
-    stroke="bold"
-    style="width:250px;height:250px">
-</lord-icon>
+
+      			<img src="../../public/wired-lineal-146-trolley.gif" className="" alt="" />
+
         <p className=' text-sm  select-none sm:text-md  mt-8 underline text-gray-700  '>There is no item in cart </p>
 
     </div>
